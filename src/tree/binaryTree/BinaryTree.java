819afd26bee01c5cfafcf6c854a2375493cdabaa
Surya B <suryaanirudh22@gmail.com>
2025-08-08 17:54:59 +0530

    added construct tree Operation in B Tree

--- src/tree/binaryTree/BinaryTree.java
+++ src/tree/binaryTree/BinaryTree.java
@@ -1,24 +1,35 @@
 package tree.binaryTree;
 
+import java.util.InputMismatchException;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
+import java.util.function.Function;
 
-public class BinaryTree<E>{
+public class BinaryTree<E> {
 
     private Node<E> root;
+    private Function<String, E> parser;
 
-    public BinaryTree(E root){
-        this.root= new Node<>(root);
+    public BinaryTree(E root) {
+        this.root = new Node<>(root);
     }
 
-    public static class Node<E>{
+    public BinaryTree(Function<String, E> parser, E root) {
+        this.parser = parser;
+        this.root = new Node<>(root);
+    }
+
+    public static class Node<E> {
         Node<E> left;
         Node<E> right;
         E element;
-        public Node(E element){
-            this.element=element;
+
+        public Node(E element) {
+            this.element = element;
         }
     }
+
     public Node<E> getRoot() {
         return root;
     }
@@ -60,17 +71,48 @@ public class BinaryTree<E>{
         System.out.print(node.element + " ");
         inOrder(node.right);
     }
-    public void preOrder(Node<E> node){
-        if(node==null) return;
-        System.out.print(node.element+" ");
+
+    public void preOrder(Node<E> node) {
+        if (node == null) return;
+        System.out.print(node.element + " ");
         preOrder(node.left);
         preOrder(node.right);
     }
-    public void postOrder(Node<E> node){
-        if (node==null) return;
+
+    public void postOrder(Node<E> node) {
+        if (node == null) return;
         postOrder(node.left);
         postOrder(node.right);
-        System.out.print(node.element+" ");
+        System.out.print(node.element + " ");
     }
 
+    public void construct(Node<E> root) {
+       Scanner sc = new Scanner(System.in);
+       try {
+           System.out.println("Do you want to insert into left of " + root.element);
+           boolean insertLeft = sc.nextBoolean();
+           if (insertLeft) {
+               System.out.println("Enter the value to insert into left of " + root.element);
+               sc.nextLine();
+               E value = readValue(sc, parser);
+               root.left = new Node<>(value);
+               construct(root.left);
+           }
+           System.out.println("Do you want to insert into right of " + root.element);
+           boolean insertRight = sc.nextBoolean();
+           if (insertRight) {
+               System.out.println("Enter the value to insert into right of " + root.element);
+               sc.nextLine();
+               E value = readValue(sc, parser);
+               root.right = new Node<>(value);
+               construct(root.right);
+           }
+       }catch (IllegalArgumentException | InputMismatchException exception){
+           System.err.println("Invalid input "+exception.getMessage());
+       }
+    }
+
+    private static <T> T readValue(Scanner sc, Function<String, T> parser) {
+        return parser.apply(sc.nextLine());
+    }
 }
--- src/tree/binaryTree/Main.java
+++ src/tree/binaryTree/Main.java
@@ -1,11 +1,45 @@
 package tree.binaryTree;
 
+import java.util.concurrent.CompletableFuture;
+
 public class Main {
     public static void main(String[] args) {
+
+        BinaryTree<Integer> integerBinaryTree = new BinaryTree<>(Integer::parseInt, 1);
+
+
+        CompletableFuture<Void> buildTreeFuture = CompletableFuture.runAsync(() -> {
+            integerBinaryTree.construct(integerBinaryTree.getRoot());
+        });
+
+        CompletableFuture<Void> traversalFuture = buildTreeFuture.thenRunAsync(() -> {
+            System.out.print("LevelOrder: ");
+            integerBinaryTree.levelOrder(integerBinaryTree.getRoot());
+            System.out.println();
+
+            System.out.print("Preorder: ");
+            integerBinaryTree.preOrder(integerBinaryTree.getRoot());
+            System.out.println();
+
+            System.out.print("Postorder: ");
+            integerBinaryTree.postOrder(integerBinaryTree.getRoot());
+            System.out.println();
+
+            System.out.print("Inorder: ");
+            integerBinaryTree.inOrder(integerBinaryTree.getRoot());
+            System.out.println();
+
+            System.out.println("Contains 2? " +
+                    integerBinaryTree.contains(integerBinaryTree.getRoot(), 2));
+            System.out.println();
+        });
+
+
         BinaryTree<String> tree = new BinaryTree<>("A");
-        tree.insertLeft(tree.getRoot(),"B");
-        BinaryTree.Node<String> cNode  = tree.insertRight(tree.getRoot(),"C");
-        tree.insertLeft(cNode,"D");
+        tree.insertLeft(tree.getRoot(), "B");
+        BinaryTree.Node<String> cNode = tree.insertRight(tree.getRoot(), "C");
+        tree.insertLeft(cNode, "D");
+
         System.out.print("Inorder: ");
         tree.inOrder(tree.getRoot());
         System.out.println();
@@ -18,6 +52,8 @@ public class Main {
         System.out.print("LevelOrder: ");
         tree.levelOrder(tree.getRoot());
         System.out.println();
-        System.out.println(tree.contains(tree.getRoot(),"C"));
+        System.out.println(tree.contains(tree.getRoot(), "C"));
+
+        traversalFuture.join();
     }
 }
